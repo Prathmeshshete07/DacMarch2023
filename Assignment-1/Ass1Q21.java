d16ba815aced1e43762e648ae36ea23713457d0b
Prathmeshshete07 <111631723+Prathmeshshete07@users.noreply.github.com>
2023-03-31 02:23:56 +0530

    Add files via upload

---
+++ Assignment-1/Ass1Q21.java
@@ -0,0 +1,14 @@
+public class Ass1Q21 {
+    public static void main(String[] args) {
+        
+        long a = 333333L;
+        Long b = new Long(a);
+        System.out.println(b.byteValue());
+        System.out.println(b.shortValue());
+        System.out.println(b.intValue());
+        System.out.println(b.floatValue());
+        System.out.println(b.doubleValue());
+        System.out.println(b.longValue());
+
+    }
+}
---
+++ Assignment-1/Ass1Q31.java
@@ -0,0 +1,10 @@
+public class Ass1Q31{
+    public static void main(String[] args) 
+    {
+        String strNumber = new String( "A12B3C" );
+         int number = Integer.parseInt(strNumber);
+        //java.lang.NumberFormatException
+ 
+         System.out.println("Number : "+number);
+}
+}
