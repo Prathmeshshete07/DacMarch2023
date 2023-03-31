d33af62681daeabdc178727dcb627261612f6533
Prathmeshshete07 <111631723+Prathmeshshete07@users.noreply.github.com>
2023-04-01 00:59:14 +0530

    Add files via upload

---
+++ OOPJ/Assignment-1/Ass1Q12.java
@@ -0,0 +1,13 @@
+public class Ass1Q12 {
+    public static void main(String[] args) {
+        
+                
+               System.out.println("The number of bits used to represent a short value : "+Short.SIZE); 
+               System.out.println("The number of bytes used to represent a short value : "+Short.BYTES); 
+               System.out.println("The minimum value a short : "+Short.MIN_VALUE); 
+               System.out.println("The maximum value a short : "+Short.MAX_VALUE); 
+            
+         
+    }
+    
+}
---
+++ OOPJ/Assignment-1/Ass1Q26.java
@@ -0,0 +1,14 @@
+public class Ass1Q26 {
+    public static void main(String args[]){
+        float a = 124;
+        float b = 120;
+        float sum = Float.sum( a, b);
+        float min = Math.min(a, b);
+        float max = Math.max( a, b);
+        System.out.println("sum of two integer numbers "+sum);
+        System.out.println("minimum number "+ min);
+        System.out.println("maximum number "+max);
+
+}
+
+}
