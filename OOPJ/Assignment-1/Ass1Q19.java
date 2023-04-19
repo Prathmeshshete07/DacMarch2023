3162676dc7d64be9aa6c4f7928c2dad026f99fd8
Prathmeshshete07 <111631723+Prathmeshshete07@users.noreply.github.com>
2023-04-19 12:24:26 +0530

    Delete OOPJ/Assignment-1 directory

--- OOPJ/Assignment-1/Ass1Q19.java
+++
@@ -1,8 +0,0 @@
-public class Ass1Q19 {
-    public static void main(String[] args) {
-        System.out.println("The number of bits used to represent a long value : "+Long.SIZE);
-        System.out.println(" The number of bytes used to represent a long value : "+Long.BYTES);
-        System.out.println("The minimum value a long : "+Long.MIN_VALUE);
-        System.out.println("The maximum value a long : "+Long.MAX_VALUE);
-    }
-}
